735c8f16ee66884b07640d7646d98d250b1afa14
hahays <diggabeat@gmail.com>
2022-05-18 11:34:30 +0300

    add project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import Clock from "./components/Clock/Clock";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [timerDays, setTimerDays] = useState();
+  const [timerHours, setTimerHours] = useState();
+  const [timerMinutes, setTimerMinutes] = useState();
+  const [timerSeconds, setTimerSeconds] = useState();
+
+  let interval;
+  const startTimer = () => {
+    const countDownDate = new Date("September 5, 2022").getTime();
+    console.log("Осталось дней :" + countDownDate);
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      console.log("Интервал между тем днем и сейчас: " + now);
+
+      const distance = countDownDate - now;
+
+      const days = Math.floor(distance / (24 * 60 * 60 * 1000));
+
+      const hours = Math.floor(
+        (distance % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60)
+      );
+
+      const minutes = Math.floor((distance % (60 * 60 * 1000)) / (1000 * 60));
+
+      const seconds = Math.floor((distance % (60 * 1000)) / 1000);
+
+      if (distance < 0) {
+        clearInterval(interval.current);
+      } else {
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    });
+  };
+
+  useEffect(() => {
+    startTimer();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Clock
+        timerDays={timerDays}
+        timerHours={timerHours}
+        timerMinutes={timerMinutes}
+        timerSeconds={timerSeconds}
+      />
     </div>
   );
 }
---
+++ src/components/Clock/Clock.js
@@ -0,0 +1,37 @@
+import React, { Fragment } from "react";
+import "./Clock.css";
+
+const Clock = ({ timerDays, timerHours, timerMinutes, timerSeconds }) => {
+  return (
+    <Fragment>
+      <div className="header">MONEY RUN OUT</div>
+      <section className="timer-container">
+        <section className="timer">
+          <div className="clock">
+            <section>
+              <p className="days">{timerDays}</p>
+              <small>Days</small>
+            </section>
+            <span>:</span>
+            <section>
+              <p className="hours">{timerHours}</p>
+              <small>Hours</small>
+            </section>
+            <span>:</span>
+            <section>
+              <p className="minutes">{timerMinutes}</p>
+              <small>Minutes</small>
+            </section>
+            <span>:</span>
+            <section>
+              <p className="seconds">{timerSeconds}</p>
+              <small>Seconds</small>
+            </section>
+          </div>
+        </section>
+      </section>
+    </Fragment>
+  );
+};
+
+export default Clock;
